0e2e7f5d19895520eca4c0510dbd0a168d2e669a
Rishikesh <rishikksh20@gmail.com>
2019-05-16 16:07:42 +0530

    for not phonemes

--- tacotron/utils/symbols.py
+++ tacotron/utils/symbols.py
@@ -8,7 +8,7 @@ from . import cmudict
 
 _pad        = '_'
 _eos        = '~'
-_characters = '!\'(),-.:;? '
+_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\'(),-.:;? '
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
